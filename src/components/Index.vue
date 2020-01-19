b35ee6bf1985b5ba6f1847dfcf73cda83a904eb8
qinuyu <1326609017@qq.com>
2020-01-19 15:02:56 +0800

    组件引入

---
+++ src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="index">
+      {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'This is my components!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-console.log("ok");
+import Vue from 'vue'
+import Index from '../components/Index'
+
+// var app = new Vue({
+//     el: '#test',
+//     data: {
+//       message: 'Hello Vue!'
+//     },
+//     created: function () {
+//         console.log('app is created')
+//       }
+//   })
+new Vue({
+    el: '#test',
+    components: { Index },
+    template: '<Index/>'
+  })
